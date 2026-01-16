1384a1b90e760559d48c4307581be3b2b545afa3
Pablo.Avila <pablo.avila@solera.com>
2026-01-16 12:36:02 +0100

    fix: thread related

--- main.py
+++ main.py
@@ -2,17 +2,14 @@
 Punto de entrada de la aplicacion
 Detector de Numeros Rojos
 """
-import sys
-from pathlib import Path
+from multiprocessing import freeze_support
 from src.ui import main
-
-# Agregar directorio raiz al path
-sys.path.insert(0, str(Path(__file__).parent))
 
 # Comprobación de Tesseract
 from src.check_tesseract import check_tesseract_installed
+
 check_tesseract_installed()
 
-
 if __name__ == "__main__":
+    freeze_support()
     main()
